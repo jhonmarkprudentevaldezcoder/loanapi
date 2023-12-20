cecabf197fa4883ef1a0aaf9f8ec1ca3f9d432e6
jhon mark valdez <111213168+jhonmarkprudentevaldezcoder@users.noreply.github.com>
2023-12-20 15:12:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -61,6 +61,24 @@ app.get("/loan/user", async (req, res) => {
   }
 });
 
+//add loan
+app.post("/loan", async (req, res) => {
+  const { email } = req.body;
+  try {
+    // Check if the email is already taken
+    const existingUser = await Loans.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already taken." });
+    }
+
+    // If the email is not taken, create the user
+    const user = await Loans.create(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  
 //login
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
